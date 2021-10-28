1eafddeba9519a356fb606b0b1c9665aca08342b
Murad-Hasanli <muradkhh@code.edu.az>
2021-10-28 19:06:51 +0400

    added files

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// background color a paragraph
+// /* function set_background() {
+//     set = document.getElementsByTagName("body")[0];
+//      myBodyElements = set.getElementsByTagName("p");
+//     myp1 = myBodyElements[0];
+//     myp1.style.background = aph"green";
+//     myp2 = myBodyElements[1];
+//  myp2.style.background = "yellow";
+//    } */
+
+    //    remove dropdown list
+// //    function removeName()
+// // {
+// // let x=document.getElementById("nameSelect");
+// // x.remove(x.selectedIndex);
+// // }
+
+// function getFormvalue()
+// {
+//   var x=document.getElementById("form1");
+//   for (var i=0;i<x.length;i++)
+//   {
+//    if (x.elements[i].value!='Submit')
+//     {  
+//       console.log(x.elements[i].value);
+//      }  
+//    }
+// }
